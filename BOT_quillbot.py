d324bf0d94ac15c1056a0de2033ffce25a39097d
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2022-08-12 16:29:27 -0300

    refactor

--- BOT_quillbot.py
+++ BOT_quillbot.py
@@ -1,28 +1,58 @@
+from mimetypes import init
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-def bot ():
-    driver = webdriver.Firefox()
-    driver.get("https://quillbot.com/")
-    time.sleep(10)
-    print("entrar no campo")
-    elem = driver.find_element(By.XPATH, "//*[@id=\"inputText\"]") 
-    print("limpar o campo")
-    elem.clear()
-    time.sleep(10)
-    print("escrever")
-    elem.send_keys("eu nao sou maluco ")
-    print("clicado")
-    time.sleep(20)
-    elem.send_keys(Keys.CONTROL + Keys.ENTER)
-    print("webscraping")
-    time.sleep(40)
-    html = driver.find_element(By.XPATH,"//*[@id=\"outputText\"]")
-    html= html.get_attribute("innerHTML")
-    print(html)
-    soup = BeautifulSoup(html, 'html.parser')
-    text = soup.get_text()
-    return text 
+
+from utilquillbot import UtilBot
+            
+class Bot():
+    def __init__(self):
+        self.elem = ""
+        self.driver = webdriver.Firefox()
+        
+    def OpenPage(self,link):
+        self.driver.get(link)
+         
+
+    def searchinput(self,input):
+        time.sleep(40)
+        print("entrar no campo")
+        self.elem = self.driver.find_element(By.XPATH,input)
+
+    def writerinput(self,text):
+        print("limpar o campo")
+        self.elem.clear()
+        time.sleep(40)
+        print("escrever")
+        self.elem.send_keys(text)
+        
+
+    def SendText(self,xpathButton):
+        time.sleep(40)
+        button = self.driver.find_element(By.XPATH,xpathButton)
+        button.click()
+        print("clicado")
+        
+
+    def Scraping(self,output):
+        time.sleep(40)
+        print("iniciar webscraping")
+        html =  self.driver.find_element(By.XPATH,output)
+        html= html.get_attribute("innerHTML")
+        soup = BeautifulSoup(html, 'html.parser')
+        self.text = soup.get_text()
+        print(self.text)
+        return self.text
     
+        
+
+
+Bot_quillbot = Bot()
+Bot_quillbot.OpenPage(UtilBot.mapquillbot["link"])
+Bot_quillbot.searchinput(UtilBot.mapquillbot["xpathinput"])
+Bot_quillbot.writerinput("eu sou louco")
+Bot_quillbot.SendText(UtilBot.mapquillbot["xpathbutton"])
+Bot_quillbot.Scraping(UtilBot.mapquillbot["xpathoutput"])
+
---
+++ utilquillbot.py
@@ -0,0 +1,15 @@
+class UtilBot:
+    
+    mapquillbot={
+        "link": "https://quillbot.com/",
+        "xpathinput": "//*[@id=\"inputText\"]",
+        "xpathbutton":"/html/body/div[1]/div[2]/div[3]/div[1]/div/div/div[1]/div/div/div/div/div/div[1]/div[2]/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div[2]/div/div/div/div/button",
+        "xpathoutput":"//*[@id=\"outputText\"]"
+    }
+
+    maptranslatebot={
+        "link": "https://translate.google.com.br/?hl=pt-BR",
+        "xpathinput": "//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[1]/span/span/div/textarea",
+        "xpathbutton":"",
+        "xpathoutput":"/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[2]/div[8]/div/div[1]/span[1]/span/span"
+    }
